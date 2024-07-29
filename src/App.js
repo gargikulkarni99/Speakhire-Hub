2bca98116f23603a3392f152a8dd47a45a35b833
gayatrikulkarni99 <gayatrikulkarni1899@gmail.com>
2024-07-29 05:34:28 -0400

    Third Commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import { Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Intern from './pages/Intern';
 import Champions from './pages/Champions';
-import './main.css';
+import '../Styles/main.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="navbar" element={<Navbar />} />
-      <Route path="intern" element={<Intern />} />
-      <Route path="champions" element={<Champions />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/intern" element={<Intern />} />
+        <Route path="/champions" element={<Champions />} />
+      </Routes>
+    </>
   );
 }
 
